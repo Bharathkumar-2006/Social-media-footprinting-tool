130ac42d101f16ec6906d31a77f41a1510d3353b
Bharathkumar-2006 <bharathkumar50033@gmail.com>
2025-08-01 22:58:07 +0530

    removed website footprinting function and the generic scraper file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from instagram_scraper import scrape_instagram
 from twitter_scraper import scrape_twitter
-from generic_scraper import scrape_website_title
 
 def main():
     print("🔍 Social Media Footprinter")
@@ -8,8 +7,7 @@ def main():
 
     print("\n[1] Instagram")
     print("[2] Twitter")
-    print("[3] Website Title")
-    print("[4] All")
+    print("[3] All")
     choice = input("Choose option: ")
 
     if choice == "1":
@@ -17,9 +15,6 @@ def main():
     elif choice == "2":
         scrape_twitter(target)
     elif choice == "3":
-        url = input("Enter URL: ")
-        scrape_website_title(url)
-    elif choice == "4":
         scrape_instagram(target)
         scrape_twitter(target)
     else:
